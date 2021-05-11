b7053c9bbcbcba2122ef4b8f2a80965646861820
Khalifan <khalifanmuwonge@gmail.com>
2021-05-11 10:51:00 -0700

    Initial commit

--- public/assets/js/fibonacci.js
+++ public/assets/js/fibonacci.js
@@ -1,28 +1,25 @@
 
-
-const returned = document.getElementsByClassName('return')[0];
-var numberInput = document.getElementsByClassName('numberF')[0];
+const returned = document.getElementById("return");
+var numberInput = document.getElementById('numberF');
 returned.addEventListener('click', (e) => {
     const number = parseInt(numberInput.value);
-    document.getElementById("returnm").innerHTML= "Output: "+ fibonacci(number);
+    document.getElementById("returnm").innerHTML = "Output: " + fibonacci(number);
     numberInput.value = "";
     e.preventDefault();
 });
 
 
-function fibonacci(num)
-{
-    var num1=0;
-    var num2=1;
+function fibonacci(num) {
+    var num1 = 0;
+    var num2 = 1;
     var sum;
-    var out =[];
-    var i=0;
-    for (i = 0; i < num; i++) 
-    {
-        sum=num1+num2;
-        num1=num2;
-        num2=sum;
-        out[i]=num2;
+    var out = [];
+    var i = 0;
+    for (i = 0; i < num; i++) {
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+        out[i] = num2;
     }
     return out;
 }
